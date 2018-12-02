a7015cfce0a54a8d67f348648358baee18543dc3
Ori <happynger@gmail.com>
2018-12-02 15:12:16 -0800

    Update

---
+++ Commands.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+public class Say : ModuleBase 
+{
+	/* !say hello => hello */
+	[Command("say"), Summary("Echoes a message.")]
+	public async Task SayCommand([Remainder, Summary("The text to echo")] string echo)
+	{
+		await ReplyAsync(echo);
+	}
+}
+
+[Group("info")]
+public class Info : ModuleBase
+{
+	/* !info square $num => $num^2 */
+	[Command("square"), Summary("Squares a number.")]
+	public async Task Square([Summary("The number to square.")] int num)
+	{
+		await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+	}
+
+	/* !info userinfo => Sans#DDDD */
+		/* !info userinfo @Khiony => Khiony#DDDD */
+		/* !info userinfo Khiony#DDDD => Khiony#DDDD */
+		/* !info userinfo Khiony => Khiony#DDDD */
+	/* !info whois $ID => Khiony#DDDD */
+	[Command("userinfo"), Summary("Returns info about the current user, or the user parameter, if one is passed.")]
+	[Alias("user", "whois")]
+	public async Task UserInfo([Summary("The (optional) user to get info for")] IUser user = null)
+	{
+		var userInfo = user ?? Context.Client.CurrentUser;
+		await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+	}
+}
--- SansMain.cs
+++ SansMain.cs
@@ -1,25 +1,38 @@
 using System;
 using System.Threading.Tasks;
+using System.Reflection;
 using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Sans
 {
 	class SansMain
 	{
+		private DiscordSocketClient _client;
+		private DiscordSocketConfig _config;
+		private CommandService _commands;
+		private IServiceProvider _services;
+
 		public static void Main(string[] args)
 			=> new SansMain().MainAsync().GetAwaiter().GetResult();
 
 		public async Task MainAsync()
 		{
-			var client = new DiscordSocketClient();
-
-			client.Log += Log;
-			client.MessageReceived += MessageReceived;
+			_config = new DiscordSocketConfig { MessageCacheSize = 100 };
+			_client = new DiscordSocketClient();
+			_commands = new CommandService();
 
 			string token = "NTE4ODcxNDU1NTAxNjQ3ODg1.DuXYxA.p5AjTuV5Wy5r2cCgNV1IK-vb9z0";
-			await client.LoginAsync(TokenType.Bot, token);
-			await client.StartAsync();
+
+			_services = new ServiceCollection().BuildServiceProvider();
+
+			_client.Log += Log;
+			await InstallCommands();
+
+			await _client.LoginAsync(TokenType.Bot, token);
+			await _client.StartAsync();
 
 			await Task.Delay(-1);
 		}
@@ -29,11 +42,30 @@ namespace Sans
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
 		}
-		
-		private async Task MessageReceived(SocketMessage message)
+
+		private async Task InstallCommands()
 		{
-			if (message.Content == "!ping")
-				await message.Channel.SendMessageAsync("Pong!");
+			_client.MessageReceived += HandleCommand;
+			await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+		}
+		
+		private async Task HandleCommand(SocketMessage messageParam)
+		{
+			int argPos = 0;
+
+			var message = messageParam as SocketUserMessage;
+			if (message == null)
+				return ;
+			
+			if (!(message.HasCharPrefix('!', ref argPos)) ||
+					message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+				return ;
+
+			var context = new CommandContext(_client, message);
+
+			var result = await _commands.ExecuteAsync(context, argPos, _services);
+			if (!result.IsSuccess)
+				await context.Channel.SendMessageAsync(result.ErrorReason);
 		}
 	}
 }
