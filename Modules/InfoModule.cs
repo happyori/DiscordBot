27e0c83bc8a9ae18d3d0e4ce1c2f511844709836
happynger <happynger@gmail.com>
2018-12-10 15:41:35 -0800

    Automated Update Dec 10 15:41:35

--- Modules/InfoModule.cs
+++ Modules/InfoModule.cs
@@ -10,13 +10,144 @@ namespace DiscordBot.Modules
 	[Group("info")]
     public class InfoModule : ModuleBase<SocketCommandContext>
     {
-
-		/* !info square $num => $num^2 */
-		[Name("Square [number]")]
-		[Command("square"), Summary("Squares a number.")]
-		public async Task Square([Summary("The number to square.")] int num)
+		[Name("Calc [Number] [Sign] [Number]")]
+		[Command("calc"), Alias("c")]
+		[Summary("Calculates an answer for you!")]
+		public async Task Calc([Remainder] string command)
 		{
-			await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+			char[] signs = new char[] {'+', '-', '*', '/', '^', '%'};
+			char sign = '\0';
+			long num1 = 0;
+			long num2 = 0;
+			long result = 0;
+
+			command = command.ToLower();
+			command = command.Trim();
+
+			var authorbuilder = new EmbedAuthorBuilder()
+			{
+				Name = Globals.msg.Author.Username,
+				IconUrl = Globals.msg.Author.GetAvatarUrl(),
+			};
+
+			var footerbuilder = new EmbedFooterBuilder()
+			{
+				Text = "Powered by your's truly"
+			};
+
+			var builder = new EmbedBuilder()
+			{
+				Color = new Color(255, 0, 135),
+				Description = "Sans de Calculato",
+				Author = authorbuilder,
+				Footer = footerbuilder,
+			};
+
+			if (!command.Contains('+') ||
+				!command.Contains('-') ||
+				!command.Contains('*') ||
+				!command.Contains('/') ||
+				!command.Contains('^') ||
+				!command.Contains('%'))
+			{
+				await UsageCalc();
+				return ;
+			}
+
+			sign = command[command.IndexOfAny(signs)];
+
+			string[] split = command.Split(sign, 2, StringSplitOptions.RemoveEmptyEntries);
+
+			if (!long.TryParse(split[0], out num1))
+			{
+				await UsageCalc();
+				return ;
+			}
+
+			if (!long.TryParse(split[1], out num2))
+			{
+				await UsageCalc();
+				return ;
+			}
+
+			if (sign == '/' && num2 == 0)
+			{
+				await ReplyAsync($"It is truly unholy to divide by 0\n{Globals.msg.Author.Mention} should be ashamed of yourself!");
+				return ;
+			}
+
+			switch (sign)
+			{
+				case '-':
+					result = num1 - num2;
+					break;
+				case '*':
+					result = num1 * num2;
+					break;
+				case '/':
+					result = num1 / num2;
+					break;
+				case '%':
+					result = num1 % num2;
+					break;
+				case '^':
+					result = (long)Math.Pow(num1, num2);
+					break;
+				default:
+					result = num1 + num2;
+					break;
+			}
+
+			builder.AddField(x =>
+			{
+				x.Name = "Calculate:";
+				x.Value = $" {num1} {sign} {num2} ";
+			});
+			builder.AddField(x =>
+			{
+				x.Name = "__RESULTS__";
+				x.Value = $"**{result}**";
+			});
+
+			await ReplyAsync("", false, builder.Build());
+		}
+
+		private async Task UsageCalc()
+		{
+			var authorbuilder = new EmbedAuthorBuilder()
+			{
+				Name = Globals.msg.Author.Username,
+				IconUrl = Globals.msg.Author.GetAvatarUrl(),
+			};
+
+			var footerbuilder = new EmbedFooterBuilder()
+			{
+				Text = "Powered by your's truly"
+			};
+
+			var builder = new EmbedBuilder()
+			{
+				Color = new Color(38, 196, 255),
+				Author = authorbuilder,
+				Footer = footerbuilder,
+			};
+			builder.AddField(x =>
+			{
+				x.Name = "Usage:";
+				x.Value = "!calc(!c) Number (+ - / * ^ %) Number";
+			});
+			builder.AddField(x =>
+			{
+				x.Name = "Example:";
+				x.Value = "!c 20 + 20";
+				x.IsInline = false;
+			});
+			builder.AddField(x =>
+			{
+				x.Name = "**RESULT**";
+				x.Value = "**40**";
+			});
+			await ReplyAsync("", false, builder.Build());
 		}
 
 		/* !info userinfo => Sans#DDDD */
